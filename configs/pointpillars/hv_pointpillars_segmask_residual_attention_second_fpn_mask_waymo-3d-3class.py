be7d49bd433b835d499d95410c5f3e135ed00064
xiaoMrzhang <563562675@qq.com>
2021-07-12 19:41:06 +0800

    add pointpillars_pillar_supervise_waymo

---
+++ configs/pointpillars/hv_pointpillars_segmask_residual_attention_second_fpn_mask_waymo-3d-3class.py
@@ -0,0 +1,10 @@
+_base_ = [
+    '../_base_/models/hv_pointpillars_pillar_supervise_second_waymo.py',
+    '../_base_/datasets/waymoD5-3d-3class.py',
+    '../_base_/schedules/schedule_2x.py',
+    '../_base_/default_runtime.py',
+]
+
+# data settings
+data = dict(train=dict(dataset=dict(load_interval=1)),
+            samples_per_gpu=2)
